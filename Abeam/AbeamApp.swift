4b31ccdadb907ff2f1ec0359d8adcb4596c908ab
Wesley Moy <wesley.moy@gmail.com>
2026-08-14 11:45:15 -0400

    Do not force app into the foreground
    Being a good citizen of the platform generally means not fighting the platform.

--- Abeam/AbeamApp.swift
+++ Abeam/AbeamApp.swift
@@ -1,25 +1,7 @@
 import SwiftUI
 
-#if os(macOS)
-import AppKit
-
-final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)
-        NSApp.activate(ignoringOtherApps: true)
-        for window in NSApp.windows {
-            window.makeKeyAndOrderFront(nil)
-        }
-    }
-}
-#endif
-
 @main
 struct AbeamApp: App {
-    #if os(macOS)
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    #endif
-
     var body: some Scene {
         WindowGroup {
             ContentView()
